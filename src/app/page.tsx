39367eaf2236f0ee269fc6554316ff84a32c41ee
Nathan <nathanferreira00@hotmail.com>
2023-10-20 15:35:58 -0300

    fetching data within component and loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const response = await fetch('http://api.github.com/users/naathanferreira')
+  const user = await response.json()
+
+  return <h1>{JSON.stringify(user, null, 2)}</h1>
 }
